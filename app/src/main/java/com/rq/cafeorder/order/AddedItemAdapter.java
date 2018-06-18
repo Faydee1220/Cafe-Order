107bea1426b7840f9ec060cccda6a72f6bbc3798
Faydee <faydee1220@gmail.com>
2018-06-18 15:53:24 +0800

    新增 iced, hot, sugar 的點擊

--- app/src/main/java/com/rq/cafeorder/order/AddedItemAdapter.java
+++ app/src/main/java/com/rq/cafeorder/order/AddedItemAdapter.java
@@ -2,19 +2,26 @@ package com.rq.cafeorder.order;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.rq.cafeorder.R;
 import com.rq.cafeorder.databinding.ItemOrderAddedItemsBinding;
+import com.rq.cafeorder.model.Constant;
 import com.rq.cafeorder.model.Item;
 
 import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Faydee on 2018/6/18.
  */
 public class AddedItemAdapter extends RecyclerView.Adapter<AddedItemAdapter.AddedItemViewHolder> {
+    private static final String TAG = AddedItemAdapter.class.getSimpleName();
 
     private OrderContract.Presenter mPresenter;
     private ArrayList<Item> mItems;
@@ -36,6 +43,7 @@ public class AddedItemAdapter extends RecyclerView.Adapter<AddedItemAdapter.Adde
         public AddedItemViewHolder(ItemOrderAddedItemsBinding binding) {
             super(binding.getRoot());
             mBinding = binding;
+            ButterKnife.bind(this, binding.getRoot());
         }
 
         private void bind(Item item) {
@@ -45,6 +53,28 @@ public class AddedItemAdapter extends RecyclerView.Adapter<AddedItemAdapter.Adde
 
         private void loadImage(String imageUrl) {
             mPresenter.loadImage(imageUrl, mBinding.imageViewOrderAddedItem);
+        }
+
+        @OnClick(R.id.radioButton_added_item_iced)
+        public void icedPressed() {
+//            Log.d(TAG, "icedPressed");
+            mItems.get(getAdapterPosition()).type = Constant.Types.ICED;
+        }
+
+        @OnClick(R.id.radioButton_added_item_hot)
+        public void hotPressed() {
+//            Log.d(TAG, "hotPressed");
+            mItems.get(getAdapterPosition()).type = Constant.Types.HOT;
+        }
+
+        @OnClick(R.id.radioButton_added_item_sugar)
+        public void sugarPressed() {
+            mItems.get(getAdapterPosition()).isSugar = true;
+        }
+
+        @OnClick(R.id.radioButton_added_item_no_sugar)
+        public void noSugarPressed() {
+            mItems.get(getAdapterPosition()).isSugar = false;
         }
     }
 
